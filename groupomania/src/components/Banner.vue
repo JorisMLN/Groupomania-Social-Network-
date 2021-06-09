7d9913c2af9b68bdfc3b9857b9bef30b733a62d6
JorisMLN <monleon.joris@gmail.com>
2021-06-09 10:58:12 +0200

    component banner (banner + avatar)

--- groupomania/src/components/Banner.vue
+++ groupomania/src/components/Banner.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="banner">
-    <h1>{{ msg }}</h1>
-    <p>Please check your work email to register you.</p>
+    <div class="photoBan">
+      <img alt="o" src="../assets/bantest.png" />
+    </div>
+    <div class="avatar">
+      <img alt="o" src="../assets/moi.jpg" />
+    </div>
   </div>
 </template>
 
@@ -9,11 +13,43 @@
 export default {
   name: "Banner",
   props: {
-    msg: String,
-  }
-}
+    // msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.banner {
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
+  .photoBan {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border: 1px solid #2c3e50;
+    height: 200px;
+    width: 100%;
+    overflow: hidden;
+    img {
+      width: 100%;
+    }
+  }
+  .avatar {
+    border: 1px solid #2c3e50;
+    height: 150px;
+    width: 150px;
+    margin-bottom: -120px;
+    border-radius: 150px;
+    overflow: hidden;
+    margin-top: -120px;
+    img {
+      width: 100%;
+      display: flex;
+      overflow: hidden;
+    }
+  }
+}
 </style>
--- groupomania/src/views/Profil.vue
+++ groupomania/src/views/Profil.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="profil">
-    <h1>This is a Profil page</h1>
-    <Banner msg="La Banner"/>
+  <div class="home">
+    <Banner msg="La banner"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+
 import Banner from '@/components/Banner.vue'
 
 export default {
-  name: 'Profil',
-  components: Banner
+  name: 'Home',
+  components: {
+    Banner,
+  }
 }
 </script>
