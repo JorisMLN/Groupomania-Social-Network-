328a65a6c20808c7cbee290736c19b36c0b09862
JorisMLN <monleon.joris@gmail.com>
2021-06-09 13:02:30 +0200

    MàJ profil + postCreation component

---
+++ groupomania/src/components/PostCreation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="PostCreation">
+    <h1> {{msg}} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostCreation",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.PostCreation {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    height: 500px;
+    width: 300px;
+    border: 1px solid #2c3e50;
+}
+</style>
--- groupomania/src/views/Profil.vue
+++ groupomania/src/views/Profil.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="home">
+  <div class="profil">
     <Banner msg="La banner"/>
     <Info msg="Les Infos"/>
+    <PostCreation msg="Que dire ?"/>
   </div>
 </template>
 
@@ -9,12 +10,24 @@
 // @ is an alias to /src
 import Banner from '@/components/Banner.vue'
 import Info from '@/components/Info.vue'
+import PostCreation from '@/components/PostCreation.vue'
 
 export default {
   name: 'Profil',
   components: {
     Banner,
-    Info
+    Info,
+    PostCreation,
   }
 }
 </script>
+
+<style scoped lang="scss">
+.profil {
+    display: flex;
+    flex: wrap;
+    justify-content: space-around;
+    flex-direction: column;
+    height: 900px;
+}
+</style>
