c5c6d844db41aa21a87f07d6c8f80238e5f50721
JorisMLN <monleon.joris@gmail.com>
2021-06-14 11:31:10 +0200

    1ere version des middlewares Multer & Auth

---
+++ backEnd/middleware/multer-config.js
@@ -0,0 +1,37 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg',
+    'image/png': 'png'
+}
+
+const storage = multer.diskStorage({
+
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    }
+});
+ 
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1000000, // 150 KB for a 1080x1080 JPG 90
+    },
+    fileFilter: (req, file, callback) => {
+        if(MIME_TYPES.hasOwnProperty(file.mimetype)) { // (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg")
+            console.log('test filter');
+            callback(null, true);
+        } else {
+            callback(null, false);
+            return callback(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    }
+});
+
+module.exports =  upload.single('image');
