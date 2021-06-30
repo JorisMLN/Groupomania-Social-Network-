37a44f86d9459a0265bfc3e159933e6a5c2aa3c4
JorisMLN <monleon.joris@gmail.com>
2021-06-30 14:55:55 +0200

    Signup, Login & info fonctionnent avec MySQL o/

--- backEnd/controllers/user.js
+++ backEnd/controllers/user.js
@@ -36,9 +36,9 @@ exports.signup = (req, res, next) => {
         password: hash, 
         lastname: req.body.lastname,
         firstname: req.body.firstname,
-        // job: req.body.job,
-        // website: req.body.website,
-        // hobbies: req.body.hobbies
+        job: req.body.job,
+        website: req.body.website,
+        hobbies: req.body.hobbies
     })
     .then(() => {console.log('Utilisateur créé !')})
     .catch(error => res.status(400).json({ error }));
@@ -73,42 +73,6 @@ exports.login = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
-
-
-
-
-
-
-/* ---------- M O N G O O S E - & - M O N G O D B ---------- */
-// const User = require('../models/user');
-
-
-exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    console.log("user:" + user._id);
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-};
-
 exports.info = (req, res, next) => {
     console.log(req.params);
     User.findOne({ _id: req.params.id })
@@ -131,22 +95,83 @@ exports.info = (req, res, next) => {
 };
 
 
-exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash,
-                lastname: req.body.lastname,
-                firstname: req.body.firstname,
-                job: req.body.job,
-                website: req.body.website,
-                hobbies: req.body.hobbies
-            });
-            console.log(user);
-            user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-};
+
+
+
+
+
+
+
+
+
+/* ---------- M O N G O O S E - & - M O N G O D B ---------- */
+// const User = require('../models/user');
+
+
+// exports.login = (req, res, next) => {
+//     User.findOne({ email: req.body.email })
+//         .then(user => {
+//             if (!user) {
+//                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+//             }
+//             bcrypt.compare(req.body.password, user.password)
+//                 .then(valid => {
+//                     if (!valid) {
+//                         return res.status(401).json({ error: 'Mot de passe incorrect !' });
+//                     }
+//                     console.log("user:" + user._id);
+//                     res.status(200).json({
+//                         userId: user._id,
+//                         token: jwt.sign(
+//                             { userId: user._id },
+//                             'RANDOM_TOKEN_SECRET',
+//                             { expiresIn: '24h' }
+//                         )
+//                     });
+//                 })
+//                 .catch(error => res.status(500).json({ error }));
+//         })
+//         .catch(error => res.status(500).json({ error }));
+// };
+
+// exports.info = (req, res, next) => {
+//     console.log(req.params);
+//     User.findOne({ _id: req.params.id })
+//         .then(user => {
+//             if (!user) {
+//                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+//             }
+//             console.log("user:" + user._id);
+//             res.status(200).json({
+//                 userId: user._id,
+//                 email: user.email,
+//                 lastname: user.lastname,
+//                 firstname: user.firstname,
+//                 job: user.job,
+//                 website: user.website,
+//                 hobbies: user.hobbies
+//             });
+//         })
+//         .catch(error => res.status(500).json({ error }));
+// };
+
+
+// exports.signup = (req, res, next) => {
+//     bcrypt.hash(req.body.password, 10)
+//         .then(hash => {
+//             const user = new User({
+//                 email: req.body.email,
+//                 password: hash,
+//                 lastname: req.body.lastname,
+//                 firstname: req.body.firstname,
+//                 job: req.body.job,
+//                 website: req.body.website,
+//                 hobbies: req.body.hobbies
+//             });
+//             console.log(user);
+//             user.save()
+//                 .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+//                 .catch(error => res.status(400).json({ error }));
+//         })
+//         .catch(error => res.status(500).json({ error }));
+// };
