2de7ff969bf20bd6e2342d4933b90de444346e23
JorisMLN <monleon.joris@gmail.com>
2021-07-06 11:35:22 +0200

    Ajout model PostLike + PostLike.create

--- backEnd/controllers/posts.js
+++ backEnd/controllers/posts.js
@@ -25,27 +25,42 @@ Post.init({
   // options
 });
 
+class PostLike extends Model {}
+PostLike.init({
+  // attributes
+  postId: { type: Sequelize.NUMBER},
+  userId: { type: Sequelize.NUMBER},
+  status: { type: Sequelize.BOOLEAN},
+}, {
+  sequelize,
+  modelname: 'postlike'
+  // options
+});
+
+
+/* ---------- R O U T E S ---------- */
+
 exports.getAllPosts = (req, res, next) => {
-    Post.findAll({
-      order: [
-        ['id', 'DESC']
-      ]
-    })
-        .then(posts => res.status(200).json(posts))
-        .catch(error => res.status(400).json({ error }));
+  Post.findAll({
+    order: [
+      ['id', 'DESC']
+    ]
+  })
+.then(posts => res.status(200).json(posts))
+.catch(error => res.status(400).json({ error }));
 };
 
 exports.createPost = (req, res, next) => {
-        console.log(req.body.post);
-        Post.create({
-            text: req.body.post.text,
-            userId: req.body.post.userId,
-            firstname: req.body.post.firstname,
-            lastname: req.body.post.lastname,
-        })
-        .then(() => res.status(201).json({ message: 'Post enregistrée !' }))
-        .catch(error => res.status(400).json({ error }));
-    };
+  console.log(req.body.post);
+  Post.create({
+    text: req.body.post.text,
+    userId: req.body.post.userId,
+    firstname: req.body.post.firstname,
+    lastname: req.body.post.lastname,
+    })
+  .then(() => res.status(201).json({ message: 'Post enregistrée !' }))
+  .catch(error => res.status(400).json({ error }));
+};
     
 exports.deletePost = (req, res, next) => {
   console.log(req.params.id);
@@ -56,142 +71,13 @@ exports.deletePost = (req, res, next) => {
 };
 
 exports.likePost = (req, res, next) => {
-  console.log(req.params.id);
-  let postId = req.params.id;
-  // Post.update({ where: {id: postId}})
-  //   .then(() => {res.status(200).json({ message: 'Post liké !'})})
-  //   .catch(error => res.status(500).json({ error }));
+  console.log(req.body.payload);
+  PostLike.create({
+    postId: req.body.payload.postId,
+    userId: req.body.payload.userId,
+    status: true,
+  })
+  .then(() => res.status(201).json({ message: 'Like enregristré!' }))
+  .catch(error => res.status(400).json({ error }));
 };
 
-    // const likeObject = req.body;
-    // Sauce.findOne({ _id: req.params.id })
-    //     .then(sauce => {
-    //         checkLikes(likeObject, sauce);
-    //         console.log("checkLikes " + sauce.likes);
-    //         Sauce.updateOne({ _id: req.params.id }, sauce)
-    //             .then(() => res.status(200).json({ message: 'objet updaté !' }))
-    //             .catch(error => res.status(400).json({ error }));
-    //     })
-
-    // function checkLikes(likeObject, sauce) {
-    //     switch (likeObject.like) {
-    //         case 1:
-    //             {
-    //                 sauce.likes++;
-    //                 sauce.usersLiked.push(likeObject.userId);
-    //                 let idIndex = sauce.usersDisliked.indexOf(likeObject.userId);
-    //                 if (idIndex > -1) {
-    //                     removeDislike(sauce, idIndex)
-    //                 }
-    //                 break;
-    //             }
-    //         case -1:
-    //             {
-    //                 sauce.dislikes++;
-    //                 sauce.usersDisliked.push(likeObject.userId);
-    //                 let idIndex = sauce.usersLiked.indexOf(likeObject.userId);
-    //                 if (idIndex > -1) {
-    //                     removeLike(sauce, idIndex)
-    //                 }
-    //                 break;
-    //             }
-    //         default:
-    //             {
-    //                 let idIndex = sauce.usersDisliked.indexOf(likeObject.userId);
-    //                 if (idIndex > -1) {
-    //                     removeDislike(sauce, idIndex)
-    //                 }
-    //                 else {
-    //                     idIndex = sauce.usersLiked.indexOf(likeObject.userId);
-    //                     removeLike(sauce, idIndex)
-    //                 }
-    //                 break;
-    //             }
-    //     }
-    // }
-
-    // function removeLike(sauce, idIndex) {
-    //     sauce.usersLiked.splice(idIndex, 1);
-    //     sauce.likes--;
-    // }
-    // function removeDislike(sauce, idIndex) {
-    //     sauce.usersDisliked.splice(idIndex, 1);
-    //     sauce.dislikes--;
-    // }
-
-
-
-/* ---------- M O N G O O S E - & - M O N G O D B ---------- */
-// const Post = require('../models/post')
-
-// exports.deletePost = (req, res, next) => {
-//     Sauce.findOne({ _id: req.params.id })
-//         .then(sauce => {
-//             const filename = sauce.imageUrl.split('/images/')[1];
-//             fs.unlink(`images/${filename}`, () => {
-//                 Sauce.deleteOne({ _id: req.params.id })
-//                     .then(() => res.status(200).json({ message: 'objet supprimé !' }))
-//                     .catch(error => res.status(400).json({ error }));
-//             });
-//         })
-//         .catch(error => res.status(500).json({ error }));
-// };
-
-// exports.likeDislikeSauce = (req, res, next) => {
-
-//     const likeObject = req.body;
-//     Sauce.findOne({ _id: req.params.id })
-//         .then(sauce => {
-//             checkLikes(likeObject, sauce);
-//             console.log("checkLikes " + sauce.likes);
-//             Sauce.updateOne({ _id: req.params.id }, sauce)
-//                 .then(() => res.status(200).json({ message: 'objet updaté !' }))
-//                 .catch(error => res.status(400).json({ error }));
-//         })
-
-//     function checkLikes(likeObject, sauce) {
-//         switch (likeObject.like) {
-//             case 1:
-//                 {
-//                     sauce.likes++;
-//                     sauce.usersLiked.push(likeObject.userId);
-//                     let idIndex = sauce.usersDisliked.indexOf(likeObject.userId);
-//                     if (idIndex > -1) {
-//                         removeDislike(sauce, idIndex)
-//                     }
-//                     break;
-//                 }
-//             case -1:
-//                 {
-//                     sauce.dislikes++;
-//                     sauce.usersDisliked.push(likeObject.userId);
-//                     let idIndex = sauce.usersLiked.indexOf(likeObject.userId);
-//                     if (idIndex > -1) {
-//                         removeLike(sauce, idIndex)
-//                     }
-//                     break;
-//                 }
-//             default:
-//                 {
-//                     let idIndex = sauce.usersDisliked.indexOf(likeObject.userId);
-//                     if (idIndex > -1) {
-//                         removeDislike(sauce, idIndex)
-//                     }
-//                     else {
-//                         idIndex = sauce.usersLiked.indexOf(likeObject.userId);
-//                         removeLike(sauce, idIndex)
-//                     }
-//                     break;
-//                 }
-//         }
-//     }
-
-//     function removeLike(sauce, idIndex) {
-//         sauce.usersLiked.splice(idIndex, 1);
-//         sauce.likes--;
-//     }
-//     function removeDislike(sauce, idIndex) {
-//         sauce.usersDisliked.splice(idIndex, 1);
-//         sauce.dislikes--;
-//     }
-// };
