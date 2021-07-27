0187a707acdf328b85e934bc88bd02433b7a14ff
JorisMLN <monleon.joris@gmail.com>
2021-07-27 10:45:13 +0200

    Add .env

--- backEnd/controllers/user.js
+++ backEnd/controllers/user.js
@@ -4,10 +4,14 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
+require('dotenv').config();
+const sqlUser = process.env.SQL_USER;
+const sqlPassword = process.env.SQL_PASSWORD;
+
 
 /* ---------- S E Q U E L I Z E - & - M Y S Q L ---------- */
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('groupomania', 'student', 'Jolisaris789', {
+const sequelize = new Sequelize('groupomania', sqlUser, sqlPassword, {
     host: 'localhost',
     dialect: 'mysql'
   });
